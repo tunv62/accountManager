20bd67e2eae251987af78777c6df054734d88f61
nguyen tu <tunv62@wru.vn>
2020-09-03 17:15:58 +0700

    daily report

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,5 @@
 const User = require('../models/user')
 
 module.exports = (req, res) => {
-    User.find({}, (err, user) => {
-        res.render('admin', {
-            data: user
-        })
-    })
+    res.render('admin')
 }
